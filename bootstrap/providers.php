a8cdada4140b6f2c3b76ec8b82ea0c61ab9a7a1a
mapo-89 <manuel@postler.de>
2025-08-25 04:53:15 +0200

    🔧 Chore: register Auth domain service provider and repository binding

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
+    App\Domain\Auth\Providers\AuthDomainServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
-    \SocialiteProviders\Manager\ServiceProvider::class,
+    SocialiteProviders\Manager\ServiceProvider::class,
 ];
